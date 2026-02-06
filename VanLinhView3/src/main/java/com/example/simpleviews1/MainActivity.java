9f47444ae838ee13cfc356e1372d2aac759117e3
Van Linh Pham <linhhacker2003@gmail.com>
2026-02-05 20:27:53 -0500

    remove the toast on open button and launching the settings scrreen when user press it

--- VanLinhView3/src/main/java/com/example/simpleviews1/MainActivity.java
+++ VanLinhView3/src/main/java/com/example/simpleviews1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.simpleviews1;
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -43,13 +44,15 @@ public class MainActivity extends AppCompatActivity {
 
 
         //---Button view---
-        Button btnOpen = (Button) findViewById(R.id.btnOpen);
-        //register the button with an event listener
-        btnOpen.setOnClickListener(new View.OnClickListener()
-        { //anonymous class
-            //implement your event handler method
+        Button btnOpen = findViewById(R.id.btnOpen);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                DisplayToast(getString(R.string.opbtn));
+                // Launch device settings
+                Intent intent = new Intent(android.provider.Settings.ACTION_SETTINGS);
+                startActivity(intent);
+
+                // Remove the toast, so nothing else happens
             }
         });
 
